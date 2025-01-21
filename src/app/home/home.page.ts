a498df4406ad9a1fc7d460297ed1b4aa336225d3
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-21 13:19:13 +0100

    Caculadora IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton, IonAlert } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton, FormsModule, IonAlert],
 })
 export class HomePage {
+  altura : number = 0;
+  peso : number = 0;
+  imc : number = 0;
+  alertButtons = ['Cerrar'];
   constructor() {}
+  calcularIMC() {
+    this.imc = this.peso / (this.altura * this.altura);
+    this.imc = parseFloat(this.imc.toFixed(2));
+    console.log(this.imc);
+  }
 }
