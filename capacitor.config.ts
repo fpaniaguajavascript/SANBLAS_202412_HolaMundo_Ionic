7c82cd77d5dfdd71630e5337dad74ac3ee932d67
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-21 12:10:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SANBLAS_202412_HolaMundo_Ionic',
+  webDir: 'www'
+};
+
+export default config;
